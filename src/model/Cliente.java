8a5c1eab56120c690863864c868fb79ac4c87f64
oTyR3D <119752546+oTyR3D@users.noreply.github.com>
2024-10-31 22:18:24 -0300

    Update Cliente.java
    Adicione ai senhora

--- src/model/Cliente.java
+++ src/model/Cliente.java
@@ -1,56 +1,66 @@
+//COMENTADO POR FILIPE S.
+
 package model;
 
 import java.sql.Date;
 
 public class Cliente {
-    private int idCliente;
-    private String nome;
-    private String email;
-    private String telefone;
-    private String dataCadastro;
+    private int idCliente;          // Identificador único do cliente
+    private String nome;            // Nome do cliente
+    private String email;           // Email do cliente
+    private String telefone;        // Telefone do cliente
+    private String dataCadastro;    // Data em que o cliente foi cadastrado
 
     // Construtor vazio
     public Cliente() {
     }
 
-    // Métodos getters e setters para acessar e modificar os atributos da classe
-
+    // Método getter para obter o ID do cliente
     public int getIdCliente() {
         return idCliente;
     }
 
+    // Método setter para modificar o ID do cliente
     public void setIdCliente(int idCliente) {
         this.idCliente = idCliente;
     }
 
+    // Método getter para obter o nome do cliente
     public String getNome() {
         return nome;
     }
 
+    // Método setter para modificar o nome do cliente
     public void setNome(String nome) {
         this.nome = nome;
     }
 
+    // Método getter para obter o email do cliente
     public String getEmail() {
         return email;
     }
 
+    // Método setter para modificar o email do cliente
     public void setEmail(String email) {
         this.email = email;
     }
 
+    // Método getter para obter o telefone do cliente
     public String getTelefone() {
         return telefone;
     }
 
+    // Método setter para modificar o telefone do cliente
     public void setTelefone(String telefone) {
         this.telefone = telefone;
     }
 
+    // Método getter para obter a data de cadastro do cliente
     public String getDataCadastro() {
         return dataCadastro;
     }
 
+    // Método setter para modificar a data de cadastro do cliente
     public void setDataCadastro(String dataCadastro) {
         this.dataCadastro = dataCadastro;
     }
